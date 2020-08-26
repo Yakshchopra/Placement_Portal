c54abfa5993b8387948ac509f5802a9615369fd4
unknown <agrawalsanskar9993@gmail.com>
2020-08-26 16:21:27 +0530

    Component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SideProfileComponent } from './side-profile/side-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    SidebarComponent,
+    SideProfileComponent
   ],
   imports: [
     BrowserModule,
