85345da9e019cf7baae32d37b716790eb2ed236c
unknown <agrawalsanskar9993@gmail.com>
2020-08-30 01:40:57 +0530

    deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  BaseUrl : 'localhost3000/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,22 +4,7 @@
 
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  BaseUrl: 'localhost3000/api/',
-  firebaseConfig : {
-    apiKey: "AIzaSyDkfMxdjlhdxJKIGaPssfMu6tPtP3Bun00",
-    authDomain: "placement-portal-9c359.firebaseapp.com",
-    databaseURL: "https://placement-portal-9c359.firebaseio.com",
-    projectId: "placement-portal-9c359",
-    storageBucket: "placement-portal-9c359.appspot.com",
-    messagingSenderId: "337269310372",
-    appId: "1:337269310372:web:65717618e05d9836457731",
-    measurementId: "G-MQ80LCL3ZW"
-  }
-  
-=======
   BaseUrl : 'localhost3000/api/'
->>>>>>> AngularInit
 };
 
 /*
