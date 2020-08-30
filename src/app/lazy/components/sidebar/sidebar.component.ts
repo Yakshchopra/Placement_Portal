17d419e53b6da46db277572ca295652fad802767
Yakshchopra <yakshchopra@gmail.com>
2020-08-30 14:05:46 +0530

    student_profile

--- src/app/lazy/components/sidebar/sidebar.component.ts
+++ src/app/lazy/components/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
