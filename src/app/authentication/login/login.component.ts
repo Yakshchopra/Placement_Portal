76ed5bd67a231f0e52aaa2c9c3fc3ac58a33f76e
Yakshchopra <yakshchopra@gmail.com>
2020-08-30 19:45:07 +0530

    login-complete

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+declare var $: any;
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,30 @@ export class LoginComponent implements OnInit {
   constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    $(document).ready(function(){
+
+      $(".a").hide();
+      $(".f").hide();
+      
+      $(".fac").click(function(){
+        $(".s").hide();
+        $(".a").hide();
+        $(".f").show();     
+      });
+
+      $(".adm").click(function(){
+        $(".s").hide();
+        $(".f").hide();
+        $(".a").show();
+      });
+
+      $(".stu").click(function(){
+        $(".a").hide();
+        $(".f").hide();
+        $(".s").show();
+      });
+      
+    });
   }
   // Login Function
   login(form) {
