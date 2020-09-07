9250633dff68ddb07e474d301a88dcd7aa602850
Yakshchopra <yakshchopra@gmail.com>
2020-09-08 01:10:06 +0530

    verifiaction

--- src/app/lazy/components/verification/verification.component.ts
+++ src/app/lazy/components/verification/verification.component.ts
@@ -2,17 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 
 
-export interface PeriodicElement {
+
+// college dataset
+export interface College {
   name: string;
-  position: number;
-  percentage: number;
-  symbol: string;
+  reg: string;
+  sem: number;
+  attachment: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', percentage: 1, symbol: 'H'},
+const COLLEGE_DATA: College[] = [
+  {name: 'Yaksh Chopra', reg: 'RA1811032010048', sem: 4, attachment:'yaksh.pdf'},
 ];
 
+// college dataset end
+
+// placement dataset
+export interface Placement{
+  name: string;
+  reg: string;
+  company: string;
+  details: string;
+}
+
+const PLACEMENT_DATA: Placement[] = [
+  {name: 'Yaksh', reg: 'RA1811032010048', details: 'Selected for round 2', company: 'Google'}
+]
+// placement dataset end
+
+// tenth dataSource
+
+  export interface Tenth{
+    name: string;
+    reg: string;
+    percentage: string;
+    attachment: string;
+  }
+
+  const TENTH_DATA: Tenth[]  = [
+    {name: 'Yaksh', reg: 'RA1811032010048', percentage: '93.2', attachment: 'pqrst.pdf'}
+  ]
+// tenthDataSource end
+
+// twelthdatasource
+export interface Twelth{
+  name: string;
+  reg: string;
+  percentage: string;
+  attachment: string;
+}
+
+const TWELTH_DATA: Twelth[]  = [
+  {name: 'Yaksh', reg: 'RA1811032010048', percentage: '93.2', attachment: 'abcdef.pdf'}
+]
+// twelthdatasource end
 
 @Component({
   selector: 'app-verification',
@@ -22,11 +65,56 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class VerificationComponent implements OnInit {
 
+  constructor() { }
 
+  ngOnInit(): void {
+    
+  }
 
-  displayedColumns: string[] = ['position', 'name', 'percentage', 'symbol']
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  // college table
 
+  displayedColumnsCollege: string[] = ['reg', 'name', 'sem', 'attachment', 'verify']
+  dataSourceCollege = new MatTableDataSource(COLLEGE_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceCollege.filter = filterValue.trim().toLowerCase();
+  }
+
+  // college table end
+
+  // placement table
+
+  displayedColumnsPlacement: string[] = ['reg', 'name', 'company', 'details', 'verify']
+  dataSourcePlacement = new MatTableDataSource(PLACEMENT_DATA);
+
+  applyFilterPlacement(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourcePlacement.filter = filterValue.trim().toLowerCase();
+  }
+
+  // placement table end
+
+  // tenth table
+    displayedColumnsTenth: string[] = ['reg', 'name', 'percentage','attachment', 'verify']
+    dataSourceTenth = new MatTableDataSource(TENTH_DATA);
+
+    applyFilterTenth(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSourceTenth.filter = filterValue.trim().toLowerCase();
+    }
+  // tenth table end
+
+  // twelth table
+    displayedColumnsTwelth: string[] = ['reg', 'name', 'percentage', 'attachment', 'verify']
+    dataSourceTwelth = new MatTableDataSource(TWELTH_DATA);
+
+    applyFilterTwelth(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSourceTwelth.filter = filterValue.trim().toLowerCase();
+    }
+
+  // twelth table end
 
   showPlacement = false;
   showCollege = true;
@@ -62,15 +150,8 @@ export class VerificationComponent implements OnInit {
   }
 
  
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+ 
 
-  constructor() { }
-
-  ngOnInit(): void {
-    
-  }
+  
 
 }
